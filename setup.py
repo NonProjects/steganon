6af45972d255cdc019b63d0bed39d96e3ecb31ff
NotStatilko <thenonproton@protonmail.com>
2024-02-01 19:13:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,31 @@
 from setuptools import setup
+from ast import literal_eval
+
+with open('steganon/version.py', encoding='utf-8') as f:
+    version = literal_eval(f.read().split('=',1)[1].strip())
 
 setup(
     name             = 'steganon',
-    version          = '0.1',
+    version          = version,
     license          = 'LGPL-2.1',
     description      = 'Steganography LSB (with PRNG by seed)',
     long_description = open('README.md', encoding='utf-8').read(),
     author           = 'NonProjects',
     author_email     = 'thenonproton@pm.me',
     url              = 'https://github.com/NonProjects/steganon',
-    packages         = ['steganon'],
+    packages         = ['steganon', 'steganon.cli'],
     install_requires = ['pillow'],
 
+    extras_require = {
+        'cli': ['click==8.1.7']
+    },
     long_description_content_type = 'text/markdown',
     keywords = ['Steganography', 'LSB', 'Cryptography'],
+
+    entry_points='''
+        [console_scripts]
+        steganon-cli=steganon.cli.steganon_cli:safe_steganon_cli_startup
+    ''',
 
     classifiers = [
         'Intended Audience :: Developers',
