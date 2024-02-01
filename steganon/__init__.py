f199217b1ba64bdc9bcdf34162f174ef66e68ddf
NotStatilko <thenonproton@protonmail.com>
2024-02-01 19:09:02 +0200

    Use version module in __init__.py

--- steganon/__init__.py
+++ steganon/__init__.py
@@ -1,11 +1,12 @@
-__version__ = '0.1'
-
 from . import api
 from . import tools
 
 from .api import LSB_WS
 from .tools import pngify
+from .version import VERSION
 
 from PIL import Image
 
+
+__version__ = VERSION
 __author__ = 'thenonproton@pm.me'
